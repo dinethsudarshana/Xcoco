aee6d5e633e685752044a64e8746fc95e97b0128
dineth sudarshana <96906803+dinethsudarshana@users.noreply.github.com>
2023-02-08 19:46:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,20 +14,20 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94764497078'] //ur owner number
-global.ownername = "Sandaru Nethsara" //ur owner name
-global.ytname = "YT: Sandaru Yt" //ur yt chanel name
-global.socialm = "GitHub: Mr-Katussa" //ur github or insta name
-global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
+global.owner = ['+1(561)2885956'] //ur owner number
+global.ownername = "Dineth sudarshana" //ur owner name
+global.ytname = "YT: sonic modZ //ur yt chanel name
+global.socialm = "GitHub: dineth sudarshana" //ur github or insta name
+global.location = "Sri Lanka, Colombo, kalutara" //ur location
 
 //bot bomdy 
-global.ownernomer = "94764497078" //ur number
-global.premium = ['94764497078'] //ur premium number
-global.botname = 'Xcoco' //ur bot name
+global.ownernomer = "+1(561)2885956" //ur number
+global.premium = ['+1(561)2885956'] //ur premium number
+global.botname = 'ʙʟᴀᴄᴋ ᴘᴀɴᴛʜᴇʀ ᴍᴅ' //ur bot name
 global.themeemoji = "♻️" //ur theme emoji
-global.packname = "Sticker By Xcoco" //ur sticker watermark packname
-global.author = "Xcoco" //ur sticker watermark author
-global.wm = "XCOCO BOT INC" //ur watermark
+global.packname = "Sticker By dineth" //ur sticker watermark packname
+global.author = "dineth" //ur sticker watermark author
+global.wm = "BLACK-PANTHER-MD BOT INC" //ur watermark
 
 // Other
 global.sessionName = 'session'
